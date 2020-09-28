84a9b1fbdd97c82d857b281f4f39f98913ca8fa1
Gravity <1987391831@qq.com>
2020-09-28 23:42:12 +0800

    完成界面切图70%

---
+++ script/index.js
@@ -0,0 +1,19 @@
+// 头部菜单栏
+const header = {
+    oMenu: document.getElementsByClassName('right-ex')[0],
+
+
+}
+
+// 滑动条
+const sliderBar = {
+    dots: document.getElementsByClassName('dot')[0].getElementsByTagName('li'),
+
+}
+
+// 列表区
+const list = {
+    
+}
+
+console.log(sliderBar.dots);
